53b1fe153bf3d2296918ae799da8300d138a968b
woongbaera <woongbaera@gmail.com>
2025-05-30 16:24:58 +0900

    suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import IngredientSearch from "@/components/IngredientSearch";
 
 export default function Home() {
@@ -5,7 +6,9 @@ export default function Home() {
     <main className="min-h-screen bg-gray-50 py-4">
       <div className="max-w-6xl mx-auto px-4">
         <h1 className="text-2xl font-bold text-center">원료성분 정보 검색</h1>
-        <IngredientSearch />
+        <Suspense>
+          <IngredientSearch />
+        </Suspense>
       </div>
     </main>
   );
